a0c3f3daa50e03d41120629e62fb1e9fe67c5d45
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-10-27 16:35:48 +0530

    Made use of Ternary operators to display comments

--- src/App.js
+++ src/App.js
@@ -14,20 +14,28 @@ function App() {
             id:3, text:'Comment three'
         },
     ]
+
+   
+    const showComments = true;
+    const commentBlock = (   
+    <div className="comments">
+    <h3>Comments ({comments.length})</h3>
+    <ul>
+        {comments.map((comment,index)=>
+        (<li key={index}>{comment.text}</li>
+        )
+    )}
+       
+    </ul>
+</div>)
+   
     return (
         <div className="container">
             <h1>{title}</h1>
             <p>{body}</p>
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                   
-                        {comments.map((comment,index)=>(
-                            <li key={index}>{comment.text}</li>
-                        ))}
-                   
-                </ul>
-            </div>
+
+            {showComments && commentBlock}
+         
         </div>
     )
 }
