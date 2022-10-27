334f1d2a3995c63b19d99965c715a8dff2b8559d
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-10-27 21:13:46 +0530

    Added Real Time Validation for form component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,25 @@ import Card from './shared/Card'
 function FeedbackForm() {
 
   const [text,setText] = useState('')
+
+  const [btnDisabled, setBtnDisbaled] = useState(true)
+  const [message, setMessage] = useState('')
   
   const handleOnChange = (event) =>{
+    if(text === ''){
+      setBtnDisbaled(true)
+      setMessage(null)
+
+    }
+
+    else if(text !== '' && text.trim().length <=10){
+      setMessage("Text must be at least 10 characters")
+      setBtnDisbaled(true)
+    }
+    else{
+      setMessage(null)
+      setBtnDisbaled(false)
+    }
     setText(event.target.value)
   }
 
@@ -18,10 +35,11 @@ function FeedbackForm() {
         {/* Rating select component */}
         <div className="input-group">
           <input type="text" onChange={handleOnChange} placeholder='Please write a review'  value={text} />
-          <Button type="submit">
+          <Button type="submit" isDisabled={btnDisabled}>
             Send
             </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
