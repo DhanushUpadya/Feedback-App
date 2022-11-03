c63c81fa40a91838d0cba014ec6308e7e288759e
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-11-03 17:21:34 +0530

    Updated and Deleted from JSON server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -38,9 +38,12 @@ export const FeedbackProvider = ({children})=>{
     }
     
 
-    const deleteFeedback = (id) =>{
+    const deleteFeedback = async(id) =>{
         if(window.confirm('Are you sure want to delete?')){
             setFeedback(feedback.filter((item)=>item.id!==id))
+            await fetch(`/feedback/${id}`,{
+                method:'DELETE'
+            })
         }
     }
 
@@ -51,9 +54,18 @@ export const FeedbackProvider = ({children})=>{
         })
     }
 
-    const updateFeedback = (id, updItem)=>{
-        setFeedback(feedback.map((item)=>item.id === id ? { ...item, ...updItem} : item))
-    }
+    const updateFeedback = async (id, updItem)=>{
+       const response= await fetch(`/feedback/${id}`,{
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(updItem)     
+        })
+        const data = await response.json()
+        setFeedback(feedback.map((item)=>item.id === id ? { ...item, ...data} : item))
+       
+    }     
 
         return ( <FeedbackContext.Provider value={{
                 feedback,
